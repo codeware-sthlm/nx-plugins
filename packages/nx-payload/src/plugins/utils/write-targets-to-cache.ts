c40d8a989e98b7f66acd320b1b3c386ab6a2d17d
github-merge-queue <118344674+github-merge-queue@users.noreply.github.com>
2024-06-22 06:01:57 +0000

    build: 📦 update nx workspace to 19.3.1

--- packages/nx-payload/src/plugins/utils/write-targets-to-cache.ts
+++
@@ -1,6 +0,0 @@
-import { type TargetConfiguration, writeJsonFile } from '@nx/devkit';
-
-export const writeTargetsToCache = (
-  cachePath: string,
-  targets: Record<string, Record<string, TargetConfiguration>>
-) => writeJsonFile(cachePath, targets);
